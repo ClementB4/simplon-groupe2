93edf36aa7dfd8c4354f1f9c537afd4a371b3dcc
clementbrunet42@gmail.com <clementbrunet42@gmail.com>
2020-10-26 11:48:38 +0100

    Clement commit

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+function picture(){
+    if(document.getElementById("image").style.display != "block"){
+        document.getElementById("image").style.display="block";
+    }else{
+        document.getElementById("image").style.display="none"
+    }
+}
