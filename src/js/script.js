a93e4e2ed5e5d8b94715250d27dacc44b3ec3637
clementbrunet42@gmail.com <clementbrunet42@gmail.com>
2020-10-27 16:22:39 +0100

    branch clean

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-function picture(){
-    if(document.getElementById("image").style.display != "block"){
-        document.getElementById("image").style.display="block";
-    }else{
-        document.getElementById("image").style.display="none"
-    }
-}
