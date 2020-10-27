63a5c07c320cea16c4025c9881a98651335baf5d
clementbrunet42@gmail.com <clementbrunet42@gmail.com>
2020-10-27 11:08:55 +0100

    correction

--- quidditch/src/js/script.js
+++ quidditch/src/js/script.js
@@ -1,19 +1,40 @@
-var position = 0;
-
-$(window).scroll(function(e) {
-  var $element = $('.header');
-  var scrollTop = $(this).scrollTop();
-  if( scrollTop <= 0 ) { 
-    $element.removeClass('hide').removeClass('scrolling');
-  } else if( scrollTop < position ) {
-    $element.removeClass('hide');
-  } else if( scrollTop > position ) {
-    $element.addClass('scrolling');
-    if( scrollTop + $(window).height() >=  $(document).height() - $element.height() ){
-      $element.removeClass('hide');
-    } else if(Math.abs($element.position().top) < $element.height()) {
-      $element.addClass('hide');
+window.onload = function() {
+    var canvas = document.getElementById('quidditch_terrain');
+    if(!canvas) {
+        alert("Impossible de récupérer le canvas");
+        return;
     }
-  }
-  position = scrollTop;
-})
+
+    var context = canvas.getContext('2d');
+    if(!context) {
+        alert("Impossible de récupérer le context du canvas");
+        return;
+    }
+    
+	function getMousePosition(canvas, event) { 
+        let rect = canvas.getBoundingClientRect(); 
+        let x = event.clientX - rect.left; 
+        let y = event.clientY - rect.top; 
+        console.log("Coordinate x: " + x,  
+                    "Coordinate y: " + y); 
+        zz = [x,y]
+        return zz
+    } 
+
+    let Vifor = new ClassVifor(canvas.width, canvas.height);
+    canvas.addEventListener("mousedown", function(e) 
+        { 
+            array = getMousePosition(canvas, e); 
+            Vifor.onclick(array[0],array[1])
+        }); 
+    
+
+	var myInterval = setInterval(animate, 1000/90);
+    function animate() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = "url('https://image.freepik.com/photos-gratuite/plein-cadre-photo-fond-cuir-marron_23-2147951253.jpg')";
+		Vifor.draw(context);
+		Vifor.colision(canvas.width, canvas.height)
+		Vifor.deplacement()
+    }
+}		
